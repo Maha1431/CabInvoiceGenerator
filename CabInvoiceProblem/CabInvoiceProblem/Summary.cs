741ccd8d9b0285f9d1fd007500bc1784ce1b9c7e
maha <mmahaece07@gmail.com>
2022-01-06 19:36:12 +0530

    UC2 Added

---
+++ CabInvoiceProblem/CabInvoiceProblem/Summary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceProblem
+{
+   public class Summary
+    {
+        public double distance;
+        public int time;
+
+        public Summary(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
