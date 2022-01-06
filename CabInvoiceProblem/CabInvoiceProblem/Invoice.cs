362eb4ee912f579dc65cf4768bd0fc4c370d9969
maha <mmahaece07@gmail.com>
2022-01-06 19:31:48 +0530

    Calculate Fare

---
+++ CabInvoiceProblem/CabInvoiceProblem/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceProblem
+{
+   public  class Invoice
+    {
+        public double CalculateFare(double distance, int time)
+        {
+            int costPerKilometer = 10;
+            int costPerMinute = 1;
+            int minimumFare = 5;
+            double totalFare = distance * costPerKilometer + time * costPerMinute;
+            if (totalFare < minimumFare)
+                return minimumFare;
+            return totalFare;
+        }
+    }
+}
