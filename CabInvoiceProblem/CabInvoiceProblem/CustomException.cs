1247094bcd16bc0e31e0f54e54321dac4c048403
maha <mmahaece07@gmail.com>
2022-01-06 19:50:01 +0530

    UC4 Added

---
+++ CabInvoiceProblem/CabInvoiceProblem/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceProblem
+{
+    public enum ExceptionType
+    {
+        NULL_EXCEPTION
+    }
+    public class CustomException : Exception
+    {
+        public string message;
+
+        public CustomException(string Message)
+        {
+            this.message = Message;
+        }
+    }
+}
---
+++ CabInvoiceProblem/CabInvoiceProblem/RideRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceProblem
+{
+   public  class RideRepository
+    {
+        public Dictionary<string, List<Summary>> userRideObject;
+
+        public RideRepository()
+        {
+            this.userRideObject = new Dictionary<string, List<Summary>>();
+        }
+
+        public void AddRides(string UserID, Summary[] rides)
+        {
+            if (UserID == null)
+                throw new CustomException(ExceptionType.NULL_EXCEPTION + "");
+            bool checkRide = userRideObject.ContainsKey(UserID);
+            List<Summary> list = new List<Summary>();
+            if (checkRide == false)
+            {
+                list.AddRange(rides);
+                userRideObject.Add(UserID, list);
+            }
+        }
+    }
+}
