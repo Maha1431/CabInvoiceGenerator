31a51f95696ec015bb2b824934d21734aaac80d8
maha <mmahaece07@gmail.com>
2022-01-06 19:40:34 +0530

    UC3 Added

---
+++ CabInvoiceProblem/CabInvoiceProblem/EnhancedInvoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceProblem
+{
+  public  class EnhancedInvoice
+    {
+        public EnhancedInvoice(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFarePerRide = totalFare / numberOfRides;
+        }
+
+        public int numberOfRides { get; }
+        public double totalFare { get; }
+        public double averageFarePerRide { get; }
+    }
+}
+}
